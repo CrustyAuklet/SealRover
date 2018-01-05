d8a3f886074dd016f2ac02c04653ef543a50a96a
Ethan Slattery <Ethan.Slattery@yahoo.com>
2018-01-05 14:31:44 -0800

    firmware update and PCB design changes (small)

--- RoverFirmware/RoverFirmware.ino
+++ RoverFirmware/RoverFirmware.ino
@@ -5,7 +5,12 @@
 #include <PWMServo.h>
 #include "SBUS.h"
 
+PWMServo throttle;
+PWMServo steering;
 PWMServo panServo;
+PWMServo tiltServo;
+PWMServo pwm_ch5;
+PWMServo pwm_ch6;
 
 // GUItool: begin automatically generated code
 AudioPlaySdWav           playSdWav1;     //xy=146,292
@@ -16,21 +21,26 @@ AudioControlSGTL5000     sgtl5000_1;     //xy=152,360
 // GUItool: end automatically generated code
 
 // Use these with the audio adaptor board
-#define SDCARD_CS_PIN    10
-#define SDCARD_MOSI_PIN  7
-#define SDCARD_SCK_PIN   14
+#define SDCARD_CS_PIN     (10)
+#define SDCARD_MOSI_PIN   (7)
+#define SDCARD_SCK_PIN    (14)
 
-// PIN DEFINES
-#define SBUS_INPUT 1
-//#define UNUSED_DIN1 2
-//#define UNUSED_DIN2 8
-#define PAN_SERVO 3
-#define TILT_SERVO 4
-//#define UNUSED_PWM2 5
-#define DIP_PIN_1 21
-#define DIP_PIN_2 20
-#define DIP_PIN_3 17
-#define DIP_PIN_4 16
+// Define Inputs
+#define SBUS_INPUT        (1)
+#define DIP_PIN_1         (15)
+#define DIP_PIN_2         (16)
+#define DIP_PIN_3         (17)
+#define BATT_SENSE        (10)
+#define TEMP_SENSE        (11)
+
+
+// Define outputs
+#define THROTTLE        (3)
+#define STEERING        (4)
+#define PAN_SERVO       (5)
+#define TILT_SERVO      (6)
+#define PWM_5           (21)
+#define PWM_6           (20)
 
 /*** GLOBAL VARIABLES ***/
 SBUS x8r(SBUS_INPUT);     // SBUS object, which is on HWSERIAL 1
@@ -40,6 +50,7 @@ uint16_t lostFrames = 0;  // counter for lost frames
 char tmpStr[30];          // temporary string for holding serial output
 float vol = .8;           // Volume setting ( range is 0 to 1, distortion when > .8 )
 short soundNum = 0;       // The number of the sound to play, set in ISR by dip-switches
+int battVal;
 
 void setup() {
     Serial.begin(9600);
@@ -64,31 +75,40 @@ void setup() {
         }
     }
     Serial.println("SD Card detected...");
+
+    // create PWM channels for motor outputs
+    throttle.attach(THROTTLE, 1000, 2000);
+    steering.attach(STEERING, 1000, 2000);
+    panServo.attach(PAN_SERVO, 500, 2500);
+    tiltServo.attach(TILT_SERVO, 800, 2200);
+    pwm_ch5.attach(PWM_5, 1000, 2000);
+    pwm_ch5.attach(PWM_6, 1000, 2000);
     
     // Setup the interrupts for DIP switches
     pinMode(DIP_PIN_1, INPUT_PULLUP);
     pinMode(DIP_PIN_2, INPUT_PULLUP);
     pinMode(DIP_PIN_3, INPUT_PULLUP);
-    pinMode(DIP_PIN_4, INPUT_PULLUP);    
     attachInterrupt(DIP_PIN_1, isrDipSwitches, CHANGE);  // Bit 1 - LSB
     attachInterrupt(DIP_PIN_2, isrDipSwitches, CHANGE);  // Bit 2
     attachInterrupt(DIP_PIN_3, isrDipSwitches, CHANGE);  // Bit 3
-    attachInterrupt(DIP_PIN_4, isrDipSwitches, CHANGE);  // Bit 4 - MSB
     isrDipSwitches();
   
 } // END OF SETUP
 
 void loop() {
+    battVal = analogRead(BATT_SENSE);
+  
     // look for a good SBUS packet from the receiver
     if(x8r.read(&channels[0], &failSafe, &lostFrames)){
-    
+
+        throttle.write(map(channels[0], 500, 1800, 0, 180));
+        steering.write(map(channels[1], 190, 1800, 180, 0));
+        panServo.write(map(channels[3], 200, 1800, 0, 180));
+        tiltServo.write(map(channels[2], 200, 1800, 0, 180));
+        
         if(channels[5] > 1000 & !playSdWav1.isPlaying()) {
           playFile(soundNum);
         }
-    
-        // Set the volume of the headphone output from 0.0 to 0.8
-        // Based on analog input on pin 15 (pot from audio shield)
-        setVolume();
     
         printInputs();
     }
@@ -125,6 +145,8 @@ void printInputs() {
     Serial.print(vol);
     Serial.print(" DIP: ");
     Serial.print(soundNum);
+    Serial.print(" BAT: ");
+    Serial.print(battVal);
     Serial.println();
 }
 
@@ -133,6 +155,5 @@ void isrDipSwitches() {
     soundNum  = !digitalRead(DIP_PIN_1);
     soundNum |= (!digitalRead(DIP_PIN_2))<<1;
     soundNum |= (!digitalRead(DIP_PIN_3))<<2;
-    soundNum |= (!digitalRead(DIP_PIN_4))<<3;
 }
 
