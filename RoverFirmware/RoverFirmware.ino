7a48cdd89e13642b263818b464c649016c1b9d68
Ethan Slattery <theslat@gmail.com>
2018-09-08 14:16:04 -0700

    updates to rover firmware, to prevent runaway acceleration on signal loss and increase scaling factors

--- RoverFirmware/RoverFirmware.ino
+++ RoverFirmware/RoverFirmware.ino
@@ -1,52 +1,29 @@
-#include <Audio.h>
 #include <Wire.h>
 #include <SPI.h>
 #include <SD.h>
+#include <Adafruit_VS1053.h>
 #include <PWMServo.h>
 #include "SBUS.h"
 
 #define DEBUG_OUTPUT 1
 
-PWMServo throttle;
-PWMServo steering;
-PWMServo panServo;
-PWMServo tiltServo;
-PWMServo pwm_ch5;
-PWMServo pwm_ch6;
-
-// GUItool: begin automatically generated code
-AudioPlaySdWav           playSdWav1;     //xy=146,292
-AudioOutputI2S           i2s1;           //xy=383,291
-AudioConnection          patchCord1(playSdWav1, 0, i2s1, 0);
-AudioConnection          patchCord2(playSdWav1, 1, i2s1, 1);
-AudioControlSGTL5000     sgtl5000_1;     //xy=152,360
-// GUItool: end automatically generated code
-
 // defines for general use and eliminating magic numbers
-#define NUM_CHANNELS      (8)
+#define NUM_CHANNELS        (8)
 
-// Use these with the audio adaptor board
-#define SDCARD_CS_PIN     (10)
-#define SDCARD_MOSI_PIN   (7)
-#define SDCARD_SCK_PIN    (14)
-
-// Define Inputs
-#define SBUS_INPUT        (1)
-#define DIP_PIN_1         (15)
-#define DIP_PIN_2         (16)
-#define DIP_PIN_3         (17)
-#define BATT_SENSE        (10)
-#define TEMP_SENSE        (11)
+// Audio board defines
+#define VS1053_RESET        (-1)  // VS1053 reset pin is not used
+#define VS1053_CS           (3)   // VS1053 chip select pin (output)
+#define VS1053_DCS          (10)  // VS1053 Data/command select pin (output)
+#define CARDCS              (8)   // SD card select pin
+#define VS1053_DREQ         (4)   // VS1053 Data request, ideally an Interrupt pin
 
 // Define outputs
-#define CENTER_VAL      (90)
-#define THROTTLE_STOP   (70)
-#define THROTTLE        (3)
-#define STEERING        (4)
-#define PAN_SERVO       (5)
-#define TILT_SERVO      (6)
-#define PWM_5           (21)
-#define PWM_6           (20)
+#define CENTER_VAL          (90)
+#define THROTTLE_STOP       (70)
+#define THROTTLE            (5)
+#define STEERING            (6)
+#define PAN_SERVO           (9)
+#define TILT_SERVO          (20)
 
 // Define SBUS channels
 #define THROTTLE_IN         (0)
@@ -59,81 +36,87 @@ AudioControlSGTL5000     sgtl5000_1;     //xy=152,360
 #define JOYSTICK_BTN_IN     (7)
 
 // Define speed scaling values
-#define SLOW_SCALE          (0.15)
-#define MEDIUM_SCALE        (0.25)
+#define SLOW_SCALE          (0.25)
+#define MEDIUM_SCALE        (0.50)
 #define FULL_SCALE          (1.00)
 #define VOLUME_SETTING      (0.80)
 
 /*** GLOBAL VARIABLES ***/
-SBUS x8r(SBUS_INPUT);                   // SBUS object, which is on HWSERIAL 1
+// music-player object for playing sound files
+Adafruit_VS1053_FilePlayer musicPlayer(VS1053_RESET, VS1053_CS, VS1053_DCS, VS1053_DREQ, CARDCS); // sound player
+PWMServo throttle;                      // PWM output for the car ESC
+PWMServo steering;                      // PWM output for the steering servo
+PWMServo panServo;                      // PWM for pan servo on camera mount
+PWMServo tiltServo;                     // PWM for pan servo on camera mount
+SBUS     x8r(Serial1);                  // SBUS object, which is on HWSERIAL 1
 uint16_t channels[16];                  // RX channels read from sbus 11 bit unsigned values (0-2047)
-char tmpStr[30];                        // temporary string for holding serial output
-uint8_t  failSafe    = 1;               // failsafe flag
-uint16_t lostFrames  = 0;               // counter for lost frames
-float vol            = VOLUME_SETTING;  // Volume setting ( range is 0 to 1, distortion when > .8 )
-short soundNum       = 0;               // The number of the sound to play, set in ISR by dip-switches
+char     tmpStr[30];                    // temporary string for holding serial output
+bool     failSafe    = true;            // failsafe flag
+bool     lostFrames  = false;           // counter for lost frames
 unsigned int panVal  = CENTER_VAL;      // tracking value for pan servo
 unsigned int tiltVal = CENTER_VAL;      // tracking value for tilt servo
 unsigned int steer   = CENTER_VAL;      // tracking value for steering
-int throt            = THROTTLE_STOP;   // tracking value for throttle
-int battVal          = 0;               // stores the battery value of the car, could be used for lipo protection
-float scaling        = 1.0;             // Value to scale the throttle by
+int      throt       = THROTTLE_STOP;   // tracking value for throttle
+int      battVal     = 0;               // stores the battery value of the car, could be used for lipo protection
+float    scaling     = 1.0;             // Value to scale the throttle by
 
 void setup() {
     Serial.begin(9600);
+    Serial.println("\n\nCosta Lab Seal Rover: v0.1.0");
+
     // begin the SBUS communication
     Serial.println("Starting SBUS input...");
     x8r.begin();
 
-    // Setup the audio
-    Serial.println("Starting Audio Controls...");
-    AudioMemory(8);
-    sgtl5000_1.enable();
-    sgtl5000_1.volume(vol);
-
-    // Make sure that SD card is inserted
-    SPI.setMOSI(SDCARD_MOSI_PIN);
-    SPI.setSCK(SDCARD_SCK_PIN);
-    if (!(SD.begin(SDCARD_CS_PIN))) {
-        // stop here, but print a message repetitively
-        while (1) {
-            Serial.println("Unable to access the SD card!");
-            delay(500);
+    // initialise the music player, print error message if not found and DON'T stop
+    if (! musicPlayer.begin()) {
+        Serial.println(F("Couldn't find VS1053, do you have the right pins defined?"));
+        musicPlayer.setVolume(10,10);
+        while(1) {
+            musicPlayer.sineTest(0x44, 500);
         }
     }
-    Serial.println("SD Card detected...");
+    else {
+        Serial.println(F("VS1053 found"));
+    }
+
+    // see if SD card exists, if not print error but DO NOT stop
+    if (!SD.begin(CARDCS)) {
+        Serial.println(F("SD failed, or not present"));
+        musicPlayer.setVolume(10,10);
+        while(1) {
+            musicPlayer.sineTest(0x44, 500);
+        }
+    }
+    else {
+        // print positive message and then list file for debug
+        Serial.println("SD OK!");
+        printDirectory(SD.open("/"), 0);
+    }
+
+    // Set volume for left, right channels. lower numbers == louder volume!
+    musicPlayer.setVolume(1,1);
+
+    // DREQ is on an interrupt pin so we can do background audio playing
+    musicPlayer.useInterrupt(VS1053_FILEPLAYER_PIN_INT);
 
     // create PWM channels for motor outputs and set starting values
     throttle.attach(THROTTLE, 1000, 2000);
     throttle.write(THROTTLE_STOP);
+
     steering.attach(STEERING, 1000, 2000);
     steering.write(CENTER_VAL);
+
     panServo.attach(PAN_SERVO, 500, 2500);
     panServo.write(CENTER_VAL);
+
     tiltServo.attach(TILT_SERVO, 800, 2200);
-    panServo.write(CENTER_VAL);
-    pwm_ch5.attach(PWM_5, 1000, 2000);
-    pwm_ch5.write(CENTER_VAL);
-    pwm_ch6.attach(PWM_6, 1000, 2000);
-    pwm_ch6.write(CENTER_VAL);
-
-    // Setup the interrupts for DIP switches
-    pinMode(DIP_PIN_1, INPUT_PULLUP);
-    pinMode(DIP_PIN_2, INPUT_PULLUP);
-    pinMode(DIP_PIN_3, INPUT_PULLUP);
-    attachInterrupt(DIP_PIN_1, isrDipSwitches, CHANGE);  // Bit 1 - LSB
-    attachInterrupt(DIP_PIN_2, isrDipSwitches, CHANGE);  // Bit 2
-    attachInterrupt(DIP_PIN_3, isrDipSwitches, CHANGE);  // Bit 3
-    isrDipSwitches();
-
-    // center the pan and tilt servo on reset
-    panServo.write(panVal);
-    tiltServo.write(tiltVal);
+    tiltServo.write(CENTER_VAL);
 
 } // END OF SETUP
 
 void loop() {
-    battVal = analogRead(BATT_SENSE);
+//    battVal = analogRead(BATT_SENSE);
 
     // look for a good SBUS packet from the receiver
     if(x8r.read(&channels[0], &failSafe, &lostFrames)){
@@ -143,21 +126,29 @@ void loop() {
         }
         else {
 
-            // Camera panning, with limits and static values
-            if(channels[CAM_PAN_IN] > 1050 && panVal < 180) {
-              panServo.write(panVal++);
-            }
-            else if(channels[CAM_PAN_IN] < 950 && panVal > 0) {
-              panServo.write(panVal--);
-            }
+            if(channels[JOYSTICK_BTN_IN] < 1500) {
+                // Camera panning, with limits and static values
+                if(channels[CAM_PAN_IN] > 1050 && panVal < 180) {
+                  panServo.write(panVal++);
+                }
+                else if(channels[CAM_PAN_IN] < 950 && panVal > 0) {
+                  panServo.write(panVal--);
+                }
 
-            // Camera Tilting, with limits and static values.
-            // mechanical limitations limits range to between 50 and 110 degrees
-            if(channels[CAM_TILT_IN] > 1050 && tiltVal < 110) {
-              tiltServo.write(tiltVal++);
+                // Camera Tilting, with limits and static values.
+                // mechanical limitations limits range to between 50 and 110 degrees
+                if(channels[CAM_TILT_IN] > 1050 && tiltVal < 110) {
+                  tiltServo.write(tiltVal++);
+                }
+                else if(channels[CAM_TILT_IN] < 950 && tiltVal > 50) {
+                  tiltServo.write(tiltVal--);
+                }
             }
-            else if(channels[CAM_TILT_IN] < 950 && tiltVal > 50) {
-              tiltServo.write(tiltVal--);
+            else {
+                panVal  = CENTER_VAL;
+                tiltVal = CENTER_VAL;
+                panServo.write(panVal);
+                tiltServo.write(tiltVal);
             }
 
             // update scaling factor
@@ -174,55 +165,51 @@ void loop() {
                 scaling = FULL_SCALE;
             }
 
-            // Map values for steering and throttle and then send them
-            throt = map(channels[THROTTLE_IN], 500, 1800, 0, 180);
-            steer = map(channels[STEERING_IN], 180, 1800, 180, 0);
+            // saftey check, throttle values should lie between 400 and 1800
+            // often sudden signal loss can result in out of range values
+            if(channels[THROTTLE_IN] > 400 && channels[THROTTLE_IN] < 1800) {
+                // Map values for steering and throttle and then send them
+                throt = map(channels[THROTTLE_IN], 500, 1800, 0, 180);
+                steer = map(channels[STEERING_IN], 180, 1800, 180, 0);
 
-            if(throt > 71){
-                throt -= THROTTLE_STOP;
-                throt *= scaling;
-                throt += THROTTLE_STOP;
+                if(throt > 71){
+                    throt -= THROTTLE_STOP;
+                    throt *= scaling;
+                    throt += THROTTLE_STOP;
+                }
+                else if(throt < 69) {
+                    throt = THROTTLE_STOP - throt;
+                    throt *= scaling;
+                    throt = THROTTLE_STOP - throt;
+                }
             }
-            else if(throt < 69) {
-                throt = THROTTLE_STOP - throt;
-                throt *= scaling;
-                throt = THROTTLE_STOP - throt;
+            else {
+                throt = THROTTLE_STOP;
+                steer = CENTER_VAL;
             }
 
             throttle.write(throt);
             steering.write(steer);
 
-            if(channels[SOUND_PLAY_IN] > 1000 & !playSdWav1.isPlaying()) {
-              playFile(soundNum);
+            if( channels[SOUND_PLAY_IN] > 1000 & !musicPlayer.playingMusic ) {
+                if(!musicPlayer.startPlayingFile("04.wav")) {
+                    Serial.println("CAN NOT FIND FILE!");
+                }
             }
         }
     }
+    else if(failSafe){
+        throttle.write(THROTTLE_STOP);
+        steering.write(CENTER_VAL);
+    }
+
     #if DEBUG_OUTPUT
     printInputs();
     #endif
 
 }
 
-// Playfile function from Teensy Example
-// plays numerically names wave files from SD card
-void playFile(const short fileNum)
-{
-  sprintf(tmpStr, "%02d.WAV", fileNum);
-  Serial.print("Playing file: ");
-  Serial.println(tmpStr);
-
-  // sketch continues to run while the file plays.
-  playSdWav1.play(tmpStr);
-
-  // A brief delay for the library read WAV info
-  delay(5);
-}
-
-void setVolume() {
-    vol = (analogRead(15) / 1024.0) * .8;
-    sgtl5000_1.volume(vol);
-}
-
+/// function to print debug output for the inputs
 void printInputs() {
     Serial.print("DATA: ");
     for(int i = 0; i < NUM_CHANNELS; i++){
@@ -234,10 +221,6 @@ void printInputs() {
     Serial.print(steer);
     Serial.print(" THROTTLE: ");
     Serial.print(throt);
-    Serial.print(" VOL: ");
-    Serial.print(vol);
-    Serial.print(" DIP: ");
-    Serial.print(soundNum);
     Serial.print(" PAN_VAL: ");
     Serial.print(panVal);
     Serial.print(" TILT_VAL: ");
@@ -251,10 +234,29 @@ void printInputs() {
     Serial.println();
 }
 
-// ISR for updating the file name when DIP switches change
-void isrDipSwitches() {
-    soundNum  = !digitalRead(DIP_PIN_1);
-    soundNum |= (!digitalRead(DIP_PIN_2))<<1;
-    soundNum |= (!digitalRead(DIP_PIN_3))<<2;
+/// File listing helper
+void printDirectory(File dir, int numTabs) {
+   while(true) {
+
+     File entry =  dir.openNextFile();
+     if (! entry) {
+       // no more files
+       //Serial.println("**nomorefiles**");
+       break;
+     }
+     for (uint8_t i=0; i<numTabs; i++) {
+       Serial.print('\t');
+     }
+     Serial.print(entry.name());
+     if (entry.isDirectory()) {
+       Serial.println("/");
+       printDirectory(entry, numTabs+1);
+     } else {
+       // files have sizes, directories do not
+       Serial.print("\t\t");
+       Serial.println(entry.size(), DEC);
+     }
+     entry.close();
+   }
 }
 
